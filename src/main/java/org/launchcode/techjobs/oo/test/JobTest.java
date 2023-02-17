12875ff26061b1b56b58d246b8f686385833c187
Dtrain112358 <dtrain11235813@protonmail.com>
2023-02-17 12:32:28 -0600

    created testJobsForQuality and testJobConstructorSetAllFields

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,7 +3,13 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Employer;
 import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.Location;
+import org.launchcode.techjobs.oo.PositionType;
+import org.launchcode.techjobs.oo.CoreCompetency;
+
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,12 +20,65 @@ public class JobTest {
 
 @Test
 public void testSettingJobId () {
-    Job job1 = new Job();
-    Job job2 = new Job();
-    String spec = "Test the Empty Constructor";
-    assertNotEquals(job1.getId(), job2.getId());
+    Job jobTest1 = new Job();
+    Job jobTest2 = new Job();
+
+    String spec = "Test the Job Class Empty Constructor";
+
+    assertNotEquals(jobTest1.getId(), jobTest2.getId());
+}
+
+//@Test
+//public void testJobConstructorSetAllFields () {
+//    Job jobTest3 = new Job("Product tester",
+//                          new Employer("ACME"),
+//                          new Location("Desert"),
+//                          new PositionType("Quality control"),
+//                          new CoreCompetency("Persistence"));
+//
+//    String spec = "Test that job class initialized all constructor parameters as required and created new objects from job fields";
+//
+//    assertsTrue(jobTest3 instanceof Job);
+//    assertsTrue(jobTest3.getEmployer() instanceof Employer);
+//    assertsTrue(jobTest3.getLocation() instanceof Location);
+//    assertsTrue(                    );
+//    assertsTrue(                    );
+//
+//    assertTrue("Product tester", name);
+//    assertTrue("ACME", jobTest3.getEmployer());
+//    assertTrue("Desert", jobTest3.getLocation());
+//    assertTrue("Quality control", jobTest3.getPositionType());
+//    assertTrue("Persistence", jobTest3.getCoreCompetency());
+//
+//    if (!assertTrue() || !assertEquals()){
+//        System.out.println(spec);
+//    }
+//
+//}
+
+
+@Test
+    public void testJobsForEquality () {
+    Job jobTest4 = new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+    Job jobTest5 = new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+    assertNotEquals(jobTest4, jobTest5);
 
 }
+
+
+
+
+
 
 }
 
