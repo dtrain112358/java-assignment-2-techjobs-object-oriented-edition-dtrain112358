4299daad069a0cf2a86419f57824babbfea99725
Dtrain112358 <dtrain11235813@protonmail.com>
2023-02-16 20:19:04 -0600

    created first test testSettingJobId and was successfully run

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,10 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +12,25 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+@Test
+public void testSettingJobId () {
+    Job job1 = new Job();
+    Job job2 = new Job();
+    String spec = "Test the Empty Constructor";
+    assertNotEquals(job1.getId(), job2.getId());
+
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
